4106782c88b2d8f096070fcfb0a99c4937e1601a
David Pusch <david.pusch@innogames.com>
2021-04-23 11:29:30 +0200

    Add functionality for meanings

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # kismet_cards
-
 Discord Bot to draw random cards from a provided set and post them to a channel for RPG purposes.
 
 # Set up
-
 I trust that you know how to setup a discord bot. Just copy the config-example.yaml to config.yaml, and fill in the appropriate values.
 
 # Images
-
 Your Images go into the images folder, or a different folder confed in the config.yaml. Default images are from the Wikipedia Article on the [Major Arcana](https://en.wikipedia.org/wiki/Major_Arcana).
+
+# Meanings
+You can submit a file with extended meaning and names for your card. See meaning-example.yaml for format.
+These will get posted together with the images.
--- bot.py
+++ bot.py
@@ -5,30 +5,44 @@ import os
 import yaml
 from random import choice
 
-def load_config():
-    with open('config.yaml') as file:
+def load_yaml(filename):
+    with open(filename) as file:
         return yaml.load(file, Loader=yaml.FullLoader)
 
 
-def draw_card(config):
+def draw_card(config, meanings):
     try:
         os.listdir(config['IMAGE_FOLDER'])
     except OSError:
         return "Please check your image folder config. Kismet can't read it."
     file_name = choice(os.listdir(config['IMAGE_FOLDER']))
-    name = file_name.split('.')[0]
-    if '_flipped' in name:
-        name = "{} flipped".format(name.split('_')[0])
+    if not meanings:
+        name = file_name.split('.')[0]
+        if '_flipped' in name:
+            name = "{} flipped".format(name.split('_')[0])
+        embed = discord.Embed()
+        embed.set_image(url="attachment://{}".format(file_name))
+        embed.set_footer(text = name)
+    else:
+        if '_' in file_name:
+            card_id = file_name.split('.')[0].split('_')[0]
+        else:
+            card_id = file_name.split('.')[0]
+        meaning = meanings[int(card_id)]
+        embed = '{} \nUpright Meaning: {}\nReversed Meaning: {}\n{}'.format(meaning['name'], meaning['upright'], meaning['reversed'], meaning['quote'])
+
     file_path = "{}/{}".format(config['IMAGE_FOLDER'], file_name)
     file = discord.File(file_path, filename=file_name)
-    embed = discord.Embed()
-    embed.set_image(url="attachment://{}".format(file_name))
-    embed.set_footer(text = name)
     return (file, embed)
 
 def main():
-    config = load_config()
+    config = load_yaml('config.yaml')
+    try:
+        meanings = load_yaml('meanings.yaml')
+    except FileNotFoundError:
+        meanings = False
     discordToken = config['DISCORD_TOKEN']
+    command_name = config['COMMAND_NAME']
     intents = discord.Intents.default()
     client = discord.Client(intents=intents)
 
@@ -43,16 +57,16 @@ def main():
     async def on_message(message):
         if message.author == client.user:
             return
-        if message.content.startswith("!kismet"):
+        if message.content.startswith(command_name):
             if "help" in message.content:
                 response = "I'm your way to tempt fate. \n" \
                            "I can do the following for you:\n" \
-                           "!kismet draw: Draw a card\n" \
-                           "!kismet: Draws a card as well\n" \
-                           "!kismet help: Show this help\n"
+                           "{} draw: Draw a card\n"\
+                           "{}: Draws a card as well\n"\
+                           "{} help: Show this help\n".format(command_name, command_name, command_name)
                 await message.channel.send(response)
-            elif message.content == "!kismet" or message.content == "!kismet draw":
-                card = draw_card(config)
+            elif message.content == command_name or message.content == "{} draw".format(command_name):
+                card = draw_card(config, meanings)
                 (image, embed) = card
                 await message.channel.send(file=image, embed=embed)
             else:
